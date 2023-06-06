57987b771ab45d75f9a25c15ffb122a09f129e20
jarrodmjack <jarrodmjack@gmail.com>
2023-06-05 21:42:17 -0400

    add posts router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,10 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   const user = useUser()
+
+  const { data } = api.posts.getAll.useQuery()
 
   return (
     <>
@@ -18,6 +19,13 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         {!!user.isSignedIn && <SignOutButton />}
+        <div>
+          {data?.map(post => (
+            <div key={post.id}>
+              {post.content}
+            </div>
+          ))}
+        </div>
       </main>
     </>
   );
