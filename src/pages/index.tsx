b1b7c8239bfbfd8a66eaf950e6407900b9794b03
jarrodmjack <jarrodmjack@gmail.com>
2023-06-06 16:27:44 -0400

    remove unused import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { SignOutButton, useUser } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
 import { RouterOutputs, api } from "~/utils/api";
