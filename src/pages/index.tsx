6512c0890f99dee7b0cf4504f9862c5631648116
jarrodmjack <jarrodmjack@gmail.com>
2023-06-07 16:03:02 -0400

    add post and profile view routes/pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ import LoadingPage from "~/components/loading/LoadingPage";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import LoadingSpinner from "~/components/loading/LoadingSpinner";
+import Link from "next/link";
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
@@ -87,10 +88,14 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-2 text-slate-300">
-          <span>{`@${author.username}`}</span>
-          <span className="font-thin">{` · ${dayjs(
-            post.createdAt
-          ).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username}`}</span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span className="font-thin">{` · ${dayjs(
+              post.createdAt
+            ).fromNow()}`}</span>
+          </Link>
         </div>
         <span className="text-2xl">{post.content}</span>
       </div>
