5451e4a1c4bde6239d84fc56a3306c0f2904c832
jarrodmjack <jarrodmjack@gmail.com>
2023-06-08 15:34:42 -0400

    fix type error

--- src/components/layout/ProfileLayout.tsx
+++ src/components/layout/ProfileLayout.tsx
@@ -1,6 +1,6 @@
 import type { PropsWithChildren } from "react";
 
-const ProfilePageLayout = (props: PropsWithChildren<{}>) => {
+const ProfilePageLayout = (props: PropsWithChildren) => {
   return (
     <main className="flex justify-center">
       <div className="h-screen w-full overflow-y-scroll border-x border-slate-400 md:max-w-2xl">
