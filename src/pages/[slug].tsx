745f87aa8e80e7177c52480b543de710f2ed1cfd
jarrodmjack <jarrodmjack@gmail.com>
2023-06-08 15:33:13 -0400

    profile page header

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -21,15 +21,19 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         <title>{data.username}</title>
       </Head>
       <ProfilePageLayout>
-        <div className="relative h-48 bg-slate-600">
+        <div className="relative h-36 bg-slate-600">
           <Image
             alt={`${data.username}'s profile picture`}
             src={data.profilePicture}
             width={128}
             height={128}
-            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black"
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
           />
-          <div>{data.username}</div>
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl font-bold">{`@${data.username ?? ""}`}</div>
+        <div className="border-b border-slate-400 w-full">
+          
         </div>
       </ProfilePageLayout>
     </>
