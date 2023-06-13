dac5f7e26ea52eede8bce4d48ca8f98311f62613
jarrodmjack <jarrodmjack@gmail.com>
2023-06-12 22:58:39 -0400

    add header

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,32 @@
+import { SignOutButton, useUser } from "@clerk/nextjs";
+import type { PropsWithChildren } from "react";
+
+const Layout = (props: PropsWithChildren) => {
+  const { isLoaded: userLoaded, isSignedIn } = useUser();
+
+  if (!userLoaded) {
+    return <div />;
+  }
+
+  return (
+    <main className="">
+      <header className="justify-between bg-green-950 py-16 text-slate-100">
+        <div className="flex justify-between container mx-auto px-40">
+          <div>
+            <h1 className="text-3xl">
+              {/* LOGO will go beside app name */}
+              FarePulse
+            </h1>
+          </div>
+          <div className="flex gap-4 items-center">
+            {isSignedIn && <SignOutButton />}
+            <span>Menu</span>
+          </div>
+        </div>
+      </header>
+      <div>{props.children}</div>
+    </main>
+  );
+};
+
+export default Layout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,92 +9,94 @@ import toast from "react-hot-toast";
 import LoadingSpinner from "~/components/loading/LoadingSpinner";
 import ProfilePageLayout from "~/components/layout/ProfileLayout";
 import PostView from "~/components/posts/PostView";
+import Layout from "~/components/layout/Layout";
+import FlightSearchForm from "~/components/forms/FlightSearchForm";
 
-const CreatePostWizard = () => {
-  const { user } = useUser();
-  const [input, setInput] = useState("");
+// const CreatePostWizard = () => {
+//   const { user } = useUser();
+//   const [input, setInput] = useState("");
 
-  if (!user) return null;
+//   if (!user) return null;
 
-  const ctx = api.useContext();
+//   const ctx = api.useContext();
 
-  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
-    onSuccess: () => {
-      setInput("");
-      void ctx.posts.getAll.invalidate();
-    },
-    onError: (e) => {
-      const errorMessage = e.data?.zodError?.fieldErrors.content;
-      const tooManyReqsMessage = e.message || null;
+//   const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+//     onSuccess: () => {
+//       setInput("");
+//       void ctx.posts.getAll.invalidate();
+//     },
+//     onError: (e) => {
+//       const errorMessage = e.data?.zodError?.fieldErrors.content;
+//       const tooManyReqsMessage = e.message || null;
 
-      if (errorMessage && errorMessage[0]) {
-        toast.error(errorMessage[0]);
-      } else if (tooManyReqsMessage) {
-        toast.error(tooManyReqsMessage);
-      } else {
-        toast.error("Error posting. Please try again");
-      }
-    },
-  });
+//       if (errorMessage && errorMessage[0]) {
+//         toast.error(errorMessage[0]);
+//       } else if (tooManyReqsMessage) {
+//         toast.error(tooManyReqsMessage);
+//       } else {
+//         toast.error("Error posting. Please try again");
+//       }
+//     },
+//   });
 
-  return (
-    <div className="flex w-full gap-4">
-      <Image
-        width={56}
-        height={56}
-        alt="Profile Image"
-        className="rounded-full"
-        src={user.profileImageUrl}
-      />
-      <input
-        placeholder="Type some emojis"
-        className="grow bg-transparent outline-none"
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        disabled={isPosting}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            e.preventDefault();
-            if (input !== "") {
-              mutate({ content: input });
-            }
-          }
-        }}
-      />
-      {input !== "" && !isPosting && (
-        <button onClick={() => mutate({ content: input })}>Post</button>
-      )}
-      {isPosting && (
-        <div className="flex items-center justify-center">
-          <LoadingSpinner size={20} />
-        </div>
-      )}
-    </div>
-  );
-};
+//   return (
+//     <div className="flex w-full gap-4">
+//       <Image
+//         width={56}
+//         height={56}
+//         alt="Profile Image"
+//         className="rounded-full"
+//         src={user.profileImageUrl}
+//       />
+//       <input
+//         placeholder="Type some emojis"
+//         className="grow bg-transparent outline-none"
+//         type="text"
+//         value={input}
+//         onChange={(e) => setInput(e.target.value)}
+//         disabled={isPosting}
+//         onKeyDown={(e) => {
+//           if (e.key === "Enter") {
+//             e.preventDefault();
+//             if (input !== "") {
+//               mutate({ content: input });
+//             }
+//           }
+//         }}
+//       />
+//       {input !== "" && !isPosting && (
+//         <button onClick={() => mutate({ content: input })}>Post</button>
+//       )}
+//       {isPosting && (
+//         <div className="flex items-center justify-center">
+//           <LoadingSpinner size={20} />
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
 
-const Feed = () => {
-  const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
+// const Feed = () => {
+//   const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
 
-  if (postsLoading) return <LoadingPage />;
+//   if (postsLoading) return <LoadingPage />;
 
-  if (!data) return <div>Something went wrong</div>;
+//   if (!data) return <div>Something went wrong</div>;
 
-  return (
-    <div className="flex flex-col">
-      {data.map((fullPost) => (
-        <PostView key={fullPost.post.id} {...fullPost} />
-      ))}
-    </div>
-  );
-};
+//   return (
+//     <div className="flex flex-col">
+//       {data.map((fullPost) => (
+//         <PostView key={fullPost.post.id} {...fullPost} />
+//       ))}
+//     </div>
+//   );
+// };
 
 const Home: NextPage = () => {
   const { isLoaded: userLoaded, isSignedIn } = useUser();
 
   // Start fetching asap
-  api.posts.getAll.useQuery();
+  // api.posts.getAll.useQuery();
 
   // Return empty div if user isn't loaded yet
   if (!userLoaded) {
@@ -102,9 +104,10 @@ const Home: NextPage = () => {
   }
 
   return (
-    <ProfilePageLayout>
-      <div className="flex border-b border-slate-400 p-4">
-        {!isSignedIn ? (
+    <Layout>
+      <div className="bg-green-950 text-slate-100 pt-8 pb-16 flex justify-center">
+        <FlightSearchForm />
+        {/* {!isSignedIn ? (
           <div>
             <SignInButton />
           </div>
@@ -112,11 +115,10 @@ const Home: NextPage = () => {
           <div>
             <SignOutButton />
           </div>
-        )}
-        {isSignedIn && <CreatePostWizard />}
+        )} */}
+        {/* {isSignedIn && <CreatePostWizard />} */}
       </div>
-      <Feed />
-    </ProfilePageLayout>
+    </Layout>
   );
 };
 
